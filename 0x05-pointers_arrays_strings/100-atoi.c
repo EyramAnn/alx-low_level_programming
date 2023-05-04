22f29165250ba5b01c243f073793fec4dab3f11e
EyramAnn <anndekonor@gmail.com>
2023-05-03 23:32:44 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,21 +8,29 @@
 
 int _atoi(char *s)
 {
-int i, j, n, x;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	i = n = 0;
-	x = 1;
-	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	while (s[c])
 	{
-		if (s[i] == '-')
-			x *= -1;
-		i++;
-	}
-	j = i;
-	while ((s[j] >= '0') && (s[j] <= '9'))
+	if (s[c] == 45)
 	{
-		n = (n * 10) + x * ((s[j]) - '0');
-		j++;
+	min *= -1;
 	}
-	return (n);
+	while (s[c] >= 48 && s[c] <= 57)
+	{
+	isi = 1;
+	ni = (ni * 10) + (s[c] - '0');
+	c++;
+	}
+	if (isi == 1)
+	{
+	break;
+	}
+	c++;
+	}
+	ni *= min;
+	return (ni);
 }
