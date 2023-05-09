e8e140edb4cee3692ef9209cdfcb580fdf1fff46
EyramAnn <anndekonor@gmail.com>
2023-05-08 20:08:28 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - redirect.
+ * @s: double the pointer to address
+ * @to: the original pointer char
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
