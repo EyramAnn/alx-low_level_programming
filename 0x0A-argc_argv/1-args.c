d2f1178e062559a71eb9ab078144b0281066397d
EyramAnn <anndekonor@gmail.com>
2023-05-12 20:46:41 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Entry point to print from
+ * @argv: The character name of code to print
+ * @argc: The character to print
+ * Return: successful
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
