9cc24375c42a33151c4b49fc8cd78fe04313ebcd
EyramAnn <anndekonor@gmail.com>
2023-05-22 19:14:27 -0700

    0x0D-preprocessor updated

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#ifndef __FILE__
+#define __FILE__
+#endif
+
+/**
+ * main - Entry point of our function
+ * Return: Always success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
